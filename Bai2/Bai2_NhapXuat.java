fdfaf0c4cbf2663216af446169e495dea320d784
Ánh Hồng <samh5813p@gmail.com>
2024-09-13 14:07:11 +0700

    Update Bai2_NhapXuat.java
    Tinh tong 2 so

--- Bai2/Bai2_NhapXuat.java
+++ Bai2/Bai2_NhapXuat.java
@@ -1,5 +1,24 @@
+package Bai2;
+
+import java.util.Scanner;
+
 class Bai2_NhapXuat{
     public static void main(String[] args) {
-        
+        // Khai bao du lieu
+        int a, b, tong;
+        // Chuan bi cho nhap du lieu
+        Scanner banPhim =new Scanner(System.in);
+        // Nhap du lieu
+            // B1: In huong dan cho user
+            System.out.println("----- CT TINH TONG 2 SO NGUYEN");
+            System.out.print("a = ");
+            // doi user nhap lay du lieu va cat vao bien a
+            a = banPhim.nextInt();
+            System.out.print("b = ");
+            // doi user nhap lay du lieu va cat vao bien b
+            b = banPhim.nextInt();
+            // Tinh tong
+            tong = a + b;
+            System.out.print("Tong = " + tong);
     }
 }

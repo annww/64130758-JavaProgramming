4103edd8521771331f8e55cf6333270b0d4ccaae
Anh Hong <samh5813p@gmail.com>
2024-09-27 15:54:42 +0700

    no message

--- BaiTapOnl/Array.java
+++ BaiTapOnl/Array.java
@@ -1,14 +1,44 @@
 import java.util.ArrayList;
 
-public class Array {
+public class Array {  // Changed class name to avoid conflict
     public static void main(String[] args) {
-            ArrayList<String> list = new ArrayList<String>();
-            list.add("Java");
-            list.add("C++");
-            list.add("PHP");
-            list.add("Python");
-            // print ra man hinh
-            System.out.println("Cac phan tu trong mang: ");
-            System.out.println(list);
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Java");
+        list.add("C++");
+        list.add("PHP");
+        
+        // Using addAll()
+        System.out.println("Phuong thuc addAll()");
+        System.out.println("-----------------------------");
+        
+        ArrayList<String> listA = new ArrayList<String>();
+        listA.addAll(list);
+        
+
+        // Show listA
+        System.out.println("list A: ");
+        showlist(listA);
+
+        // retainAll
+        System.out.println("Phuong thuc retainAll()");
+        System.out.println("-----------------------------");
+        ArrayList<String> listB = new ArrayList<String>();
+        listB.add("Java");
+        listA.retainAll(listB);
+        System.out.println("list A after retainAll: ");
+        showlist(listA);
+
+        // RemoveALL
+        System.out.println("Phuong thuc removeAll()");
+        System.out.println("------------------------");
+        list.removeAll(listB);
+        System.out.println("list after remove all: ");
+        showlist(list);
+    }
+    public static void showlist(ArrayList<String> list){
+        for(String obj:list){
+            System.out.println("\t" + obj);
+        }
+        System.out.println("\n");
     }
 }

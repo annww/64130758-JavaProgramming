52f087b30f2fa0d61f680ad5356963fb95cf06af
Anh Hong <samh5813p@gmail.com>
2024-11-15 13:41:04 +0700

    no message

--- BaiTapTuanHocSo/baitaptuanhocso/src/main/java/com/example/baitaptuanhocso/HelloApplication.java
+++ BaiTapTuanHocSo/baitaptuanhocso/src/main/java/com/example/baitaptuanhocso/HelloApplication.java
@@ -24,6 +24,14 @@ public class HelloApplication extends Application {
       gc.setStroke(Color.BLACK);
       gc.strokeOval(150, 150, 100, 100);
 
+      // Ve hinh tam giac
+      gc.setLineWidth(2);
+      gc.setFill(Color.WHITE);
+      double[] X_position = {100,200,300};
+      double[] Y_position = {160,80,160};
+      gc.strokePolygon(X_position,Y_position,3);
+      gc.fillPolygon(X_position,Y_position,3);
+
       // Them vao scene
       Group root = new Group(canvas);
       Scene scene = new Scene(root, 400, 400);

0f6975e66f0411b1d57bcfdb61f6a9d4f8d33735
Anh Hong <samh5813p@gmail.com>
2024-12-06 14:48:53 +0700

    Update giao dien bai3

--- BaiThucHanh/BaiThucHanh3_FlashQuizGUI/src/main/java/th/duongthianhhong/baithuchanh3_flashquizgui/QuizApplication.java
+++ BaiThucHanh/BaiThucHanh3_FlashQuizGUI/src/main/java/th/duongthianhhong/baithuchanh3_flashquizgui/QuizApplication.java
@@ -11,7 +11,7 @@ public class QuizApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(QuizApplication.class.getResource("FlashQuizGUI.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    Scene scene = new Scene(fxmlLoader.load());
     stage.setTitle("Hello!");
     stage.setScene(scene);
     stage.show();
